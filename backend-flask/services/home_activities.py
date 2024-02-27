c3c06242211ff6dc5b2f73a8bbeda5a054598d12
Edwin Ugwu <edwinbuike@gmail.com>
2024-02-27 02:17:29 +0100

    correction updates

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,29 +2,20 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 import logging
 
-from lib.db import pool, query_wrap_object, query_wrap_array
+from lib.db import db
 
-tracer = trace.get_tracer("home.activities")
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run(logger):
-    logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activities-trace-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
-      span.set_attribute("app.result_length", len(results))
+  #def run(logger):
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activities-trace-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    #  span.set_attribute("app.result_length", len(results))
 
-      sql = query_wrap_array("""
-      SELECT * FROM activities
-      """)
-      print(sql)
-      print("SQL--------------")
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      print("==================")
-      return json[0]
+  def run(cognito_user_id=None):
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
